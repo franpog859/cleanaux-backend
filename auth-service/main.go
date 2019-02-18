a478373ce15bc96d3889057c92f7c33b7451a61c
franpog859 <franciszek.pog@gmail.com>
2019-02-18 19:26:08 +0100

    Add simple authorization service

---
+++ auth-service/main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"math/rand"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+type user struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Token    string `json:"token"`
+}
+
+var users = make(map[string]string)
+
+var seedUsers = []user{
+	user{
+		Username: "user1",
+		Password: "pass1",
+	},
+	user{
+		Username: "user2",
+		Password: "pass2",
+	},
+	user{
+		Username: "user3",
+		Password: "pass3",
+	},
+}
+
+const port = ":8001"
+
+func main() {
+	gin.SetMode(gin.ReleaseMode)
+
+	router := gin.Default()
+	router.POST("/login", login)
+	router.POST("/logout", logout)
+	router.POST("/authenticate", authenticate)
+	router.Run(port)
+}
+
+func login(context *gin.Context) {
+	username := context.PostForm("username")
+	password := context.PostForm("password")
+
+	token := validateUser(username, password)
+
+	if token != "" {
+		users[username] = token
+		context.JSON(http.StatusOK, gin.H{"token": token})
+	} else {
+		context.AbortWithStatus(http.StatusUnauthorized)
+	}
+}
+
+func logout(context *gin.Context) {
+	username := context.PostForm("username")
+	token := context.PostForm("token")
+
+	value, ok := users[username]
+	if ok && value == token {
+		delete(users, username)
+		context.JSON(http.StatusOK, nil)
+	} else {
+		context.AbortWithStatus(http.StatusUnauthorized)
+	}
+}
+
+func authenticate(context *gin.Context) {
+	username := context.PostForm("username")
+	token := context.PostForm("token")
+
+	value, ok := users[username]
+	if ok && value == token {
+		context.JSON(http.StatusOK, nil)
+	} else {
+		context.AbortWithStatus(http.StatusUnauthorized)
+	}
+}
+
+func validateUser(username, password string) string {
+	// TODO: Use database storing users.
+	for _, user := range seedUsers {
+		if username == user.Username {
+			if password == user.Password {
+				return generateSessionToken()
+			}
+			return ""
+		}
+	}
+	return ""
+}
+
+func generateSessionToken() string {
+	// TODO: Create more secure token
+	return strconv.FormatInt(rand.Int63(), 16)
+}
