fb8f6b7e903253383662cf029d62e3d3ccf0f1b4
franpog859 <franciszek.pog@gmail.com>
2019-02-19 21:17:05 +0100

    Add simple kubernetes deployments

--- test/integration-test.sh
+++ test/integration-test.sh
@@ -4,7 +4,7 @@ cd ../auth-service
 docker build -t auth-service .
 docker run -d -p 8001:8001 auth-service
 
-cd ../web-service
+cd ../entry-service
 go test -tags integration
 
 # TODO: Somehow delete running container.
