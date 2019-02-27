7f74000d56d06565e58b927273ba61e835ba41fa
franpog859 <franciszek.pog@gmail.com>
2019-02-27 12:25:42 +0100

    Create go structure and update tests

--- test/integration-test.sh
+++ test/integration-test.sh
@@ -5,6 +5,6 @@ docker build -t auth-service .
 CONTAINER="$(docker run -d -p 8000:8000 auth-service)"
 
 cd ../entry-service
-go test -tags integration
+go test -tags integration ./...
 
 docker kill ${CONTAINER}
