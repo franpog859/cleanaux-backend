177cd7133a88b162d8482ea742b7b34e83090639
franpog859 <franciszek.pog@gmail.com>
2019-03-19 12:32:27 +0100

    Fix bugs with init container

--- content-service/init/db-test.sql
+++ content-service/init/db-test.sql
@@ -3,5 +3,5 @@ INSERT INTO users(name, points, lastUsageDate) VALUES
 ("testUser", 2, "2019-02-02");
 
 INSERT INTO items(name, lastUserId, lastUsageDate, intervalDays) VALUES
-("kitchen", 0, "2019-03-03", 4),
-("bathroom", 0, "2019-03-02", 4);
+("kitchen", 1, "2019-03-03", 4),
+("bathroom", 1, "2019-03-02", 4);
