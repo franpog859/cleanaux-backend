82ba18b1f51d59f7d09e96ada93b7f97c2f211b1
franpog859 <franciszek.pog@gmail.com>
2019-05-24 22:37:07 +0200

    Update persisten-volumes provisioning on GCP

--- scripts/install-gcp.sh
+++ scripts/install-gcp.sh
@@ -5,12 +5,18 @@ set -o errexit ; set -o nounset
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 KUBERNETES_DIR="${CURRENT_DIR}/../kube"
 
-echo "Applying resources..."
+echo "Applying services..."
 kubectl apply -f ${KUBERNETES_DIR}/entry-service/
 kubectl apply -f ${KUBERNETES_DIR}/auth-service/
 kubectl apply -f ${KUBERNETES_DIR}/content-service/
-kubectl apply -f ${KUBERNETES_DIR}/mysql-database/
-kubectl apply -f ${KUBERNETES_DIR}/mongo-database/
+
+echo "Applying databases..."
+kubectl apply -f ${KUBERNETES_DIR}/mysql-database/persistent-volume-gcp.yaml
+kubectl apply -f ${KUBERNETES_DIR}/mysql-database/deployment.yaml
+kubectl apply -f ${KUBERNETES_DIR}/mysql-database/service.yaml
+kubectl apply -f ${KUBERNETES_DIR}/mongo-database/persistent-volume-gcp.yaml
+kubectl apply -f ${KUBERNETES_DIR}/mongo-database/deployment.yaml
+kubectl apply -f ${KUBERNETES_DIR}/mongo-database/service.yaml
 
 echo "Setting up ingress..."
 kubectl create clusterrolebinding cluster-admin-binding --clusterrole cluster-admin --user $(gcloud config get-value account)
