d3fb2cd056ef4235d9aeb734b6ffac3d32f8dd22
Franciszek Pogodziński <32485355+franpog859@users.noreply.github.com>
2019-08-16 17:13:25 +0200

    Minify MySQL architecture (#12)

--- content-service/init/db-test.sql
+++ content-service/init/db-test.sql
@@ -1,9 +1,7 @@
 USE content;
 
-INSERT INTO users(name, points, lastUsageDate) VALUES
-("testUser", 3, "2019-01-01"),
-("testUser", 2, "2019-02-02");
+INSERT INTO items(name, lastUsageDate, intervalDays) VALUES
+("kitchen", "2019-03-03", 4),
+("bathroom", "2019-03-02", 4);
 
-INSERT INTO items(name, lastUserId, lastUsageDate, intervalDays) VALUES
-("kitchen", 1, "2019-03-03", 4),
-("bathroom", 1, "2019-03-02", 4);
+EXIT
